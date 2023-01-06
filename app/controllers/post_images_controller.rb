5ab509d96b8f6f6ef722a54cbb3a1fdde4fdf759
Threeestone <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-01-06 13:22:22 +0000

    [start] meshiterro 10

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,14 @@
+class PostImagesController < ApplicationController
+ def new
+  @post_image = Post_Image.new
+ end
+
+ def index
+
+ end
+
+ def show
+
+ end
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+　root to: 'homes#top'
+  devise_for :users
+  
+ resources :post_images, only: [:new, :index, :show]
+  
+ get "/homes/about" => "homes#about", as: "about"
+end
