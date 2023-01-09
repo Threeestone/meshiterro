986b21ec35f876cc34349669ff5e6fdbef853e07
Threeestone <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-01-09 00:54:08 +0000

    [finish]meshiterro 10,11

--- db/migrate/20230103081140_create_post_images.rb
+++ db/migrate/20230103081140_create_post_images.rb
@@ -2,6 +2,7 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
       t.intager   :id
+      t.string    :shop_name
       t.text      :caption
       t.intager   :user_id
       t.timestamps
